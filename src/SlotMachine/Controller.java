2900c33de010e86faecb74400a202096c5aa29d9
PaddyHayes <55146192+PaddyHayes@users.noreply.github.com>
2019-11-19 17:43:27 +0000

    Add files via upload

---
+++ src/SlotMachine/Controller.java
@@ -0,0 +1,19 @@
+package SlotMachine;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.media.*;
+import javax.swing.*;
+
+public class Controller
+{
+    @FXML
+    private Button button1;
+
+    public void click()
+    {
+        JOptionPane.showMessageDialog(null, "What a coincidance");
+    }
+
+
+}
