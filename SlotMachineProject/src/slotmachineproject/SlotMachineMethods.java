1fce4fc91873afde54467a9cf4f0232758797428
PaddyHayes <55146192+PaddyHayes@users.noreply.github.com>
2019-11-28 19:43:19 +0000

    Add files via upload
    Slotmachine coin button

---
+++ SlotMachineProject/src/slotmachineproject/SlotMachineMethods.java
@@ -0,0 +1,24 @@
+package slotmachineproject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Paddy
+ */
+public class SlotMachineMethods {
+    int playerMoney = 0;
+
+public int addMoney(int playerMoney){
+    playerMoney++;
+    
+    return playerMoney;
+
+}
+    
+    
+}
