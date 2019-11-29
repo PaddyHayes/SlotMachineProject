3108336efcf3c1f7d30d793e28b2104862dfdd41
PaddyHayes <55146192+PaddyHayes@users.noreply.github.com>
2019-11-29 13:01:48 +0000

    Add files via upload
    fixed the annoyance

--- SlotMachineProject/src/slotmachineproject/SlotMachineGUI.java
+++ SlotMachineProject/src/slotmachineproject/SlotMachineGUI.java
@@ -33,51 +33,34 @@ public class SlotMachineGUI  extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jFrame1 = new javax.swing.JFrame();
-        jButton1 = new javax.swing.JButton();
-        jFrame2 = new javax.swing.JFrame();
-        insertCoinbtn = new javax.swing.JButton();
+        firstSlotPlacelbl = new javax.swing.JLabel();
+        slotMachinelbl = new javax.swing.JLabel();
         slotMachinebtn = new javax.swing.JButton();
-
-        jButton1.setText("Insert Coin");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
-        jFrame1.getContentPane().setLayout(jFrame1Layout);
-        jFrame1Layout.setHorizontalGroup(
-            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jFrame1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addContainerGap(305, Short.MAX_VALUE))
-        );
-        jFrame1Layout.setVerticalGroup(
-            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jFrame1Layout.createSequentialGroup()
-                .addContainerGap(253, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(24, 24, 24))
-        );
-
-        jButton1.getAccessibleContext().setAccessibleName("insertCoinBtn");
-        jButton1.getAccessibleContext().setAccessibleDescription("");
-
-        javax.swing.GroupLayout jFrame2Layout = new javax.swing.GroupLayout(jFrame2.getContentPane());
-        jFrame2.getContentPane().setLayout(jFrame2Layout);
-        jFrame2Layout.setHorizontalGroup(
-            jFrame2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        jFrame2Layout.setVerticalGroup(
-            jFrame2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        insertCoinbtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(564, 524));
+        setSize(new java.awt.Dimension(200, 200));
+        getContentPane().setLayout(null);
+
+        firstSlotPlacelbl.setIcon(new javax.swing.ImageIcon(getClass().getResource("/slotmachineproject/images/slotNum7.png"))); // NOI18N
+        firstSlotPlacelbl.setBounds(40, 20, 20, 20);
+        getContentPane().add(firstSlotPlacelbl);
+        firstSlotPlacelbl.setBounds(190, 220, 50, 64);
+
+        slotMachinelbl.setIcon(new javax.swing.ImageIcon(getClass().getResource("/slotmachineproject/images/slotMachine.png"))); // NOI18N
+        getContentPane().add(slotMachinelbl);
+        slotMachinelbl.setBounds(150, 130, 240, 200);
+
+        slotMachinebtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/slotmachineproject/images/leverUp.png"))); // NOI18N
+        slotMachinebtn.setToolTipText("");
+        slotMachinebtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                slotMachinebtnActionPerformed(evt);
+            }
+        });
+        getContentPane().add(slotMachinebtn);
+        slotMachinebtn.setBounds(390, 130, 81, 260);
 
         insertCoinbtn.setBackground(new java.awt.Color(20, 20, 20));
         insertCoinbtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/slotmachineproject/images/coin2.png"))); // NOI18N
@@ -89,50 +72,13 @@ public class SlotMachineGUI  extends javax.swing.JFrame {
                 insertCoinbtnActionPerformed(evt);
             }
         });
-
-        slotMachinebtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/slotmachineproject/images/leverUp.png"))); // NOI18N
-        slotMachinebtn.setToolTipText("");
-        slotMachinebtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                slotMachinebtnActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(36, 36, 36)
-                .addComponent(insertCoinbtn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 460, Short.MAX_VALUE)
-                .addComponent(slotMachinebtn, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(0, 53, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(slotMachinebtn, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(insertCoinbtn, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-        );
-
-        slotMachinebtn.getAccessibleContext().setAccessibleName("");
+        getContentPane().add(insertCoinbtn);
+        insertCoinbtn.setBounds(150, 330, 50, 50);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void insertCoinbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_insertCoinbtnActionPerformed
-        // TODO add your handling code here:
-        smm.addMoney(1);
-    }//GEN-LAST:event_insertCoinbtnActionPerformed
-
     private void slotMachinebtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_slotMachinebtnActionPerformed
         // TODO add your handling code here:
         if(smm.getMoney()>=1)
@@ -141,31 +87,35 @@ public class SlotMachineGUI  extends javax.swing.JFrame {
             smm.addMoney(-1);
             JOptionPane.showMessageDialog(null,"Current money: " + smm.getMoney());
         }
-        
+
         else
         {
             JOptionPane.showMessageDialog(null, "No money Fam");
         }
-        
+
         if(evt.getSource()==slotMachinebtn){
-        
-        try
-        {
-            //slotMachinebtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/slotmachineproject/images/leverUp.png")));
-        Image leverDown = ImageIO.read(getClass().getResource("/slotmachineproject/images/leverDown.png"));
-        slotMachinebtn.setIcon(new ImageIcon(leverDown));
-        Thread.sleep(0);
-        Image leverUp = ImageIO.read(getClass().getResource("/slotmachineproject/images/images/leverUp.png"));
-        slotMachinebtn.setIcon(new ImageIcon(leverUp));
-        
-        
-        }
-        catch(Exception ex)
-        {
-            System.out.print(ex);
-        }
+            //got help from Ryan Madigan (3rd year games dev)
+            try
+            {
+                //slotMachinebtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/slotmachineproject/images/leverUp.png")));
+                Image leverDown = ImageIO.read(getClass().getResource("/slotmachineproject/images/leverDown.png"));
+                slotMachinebtn.setIcon(new ImageIcon(leverDown));
+                Thread.sleep(0);
+                Image leverUp = ImageIO.read(getClass().getResource("/slotmachineproject/images/images/leverUp.png"));
+                slotMachinebtn.setIcon(new ImageIcon(leverUp));
+
+            }
+            catch(Exception ex)
+            {
+                System.out.print(ex);
+            }
         }
     }//GEN-LAST:event_slotMachinebtnActionPerformed
+
+    private void insertCoinbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_insertCoinbtnActionPerformed
+        // TODO add your handling code here:
+        smm.addMoney(1);
+    }//GEN-LAST:event_insertCoinbtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -203,10 +153,9 @@ public class SlotMachineGUI  extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel firstSlotPlacelbl;
     private javax.swing.JButton insertCoinbtn;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JFrame jFrame1;
-    private javax.swing.JFrame jFrame2;
     private javax.swing.JButton slotMachinebtn;
+    private javax.swing.JLabel slotMachinelbl;
     // End of variables declaration//GEN-END:variables
 }
